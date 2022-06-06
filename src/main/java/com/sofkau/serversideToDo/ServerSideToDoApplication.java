89fd8850e92bed00ebbcc6f6be2cb77199c54c4d
Sherilyn Maestre <you@example.com>
2022-06-06 15:03:02 -0500

    Initializing project

---
+++ src/main/java/com/sofkau/serversideToDo/ServerSideToDoApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.serversideToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSideToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSideToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofkau/serversideToDo/ServerSideToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.serversideToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSideToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
